d447d35e10aa8368dde2670cfc072aa5e1c13be8
LMH01 <lmh-01@netcologne.de>
2025-03-23 17:26:42 +0100

    fixed flake build

--- flake.nix
+++ flake.nix
@@ -1,72 +1,94 @@
 {
-  description = "A compiler, runtime environment and debugger for an assembly-like programming language called Alpha-Notation";
+  description = "Simple program that can check programs for updates.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
     crane.url = "github:ipetkov/crane";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs:
-    inputs.flake-parts.lib.mkFlake
+  outputs = { self, nixpkgs, crane, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        inherit (pkgs) lib;
+
+        craneLib = crane.mkLib pkgs;
+
+        unfilteredRoot = ./.; # The original, unfiltered source
+        src = lib.fileset.toSource {
+          root = unfilteredRoot;
+          fileset = lib.fileset.unions [
+            # Default files from crane (Rust and cargo files)
+            (craneLib.fileset.commonCargoSources unfilteredRoot)
+            # Include all the .sql migrations as well
+            ./migrations
+          ];
+        };
+
+        # Common arguments can be set here to avoid repeating them later
+        # Note: changes here will rebuild all dependency crates
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+
+          buildInputs = with pkgs; [
+            # Add additional build inputs here
+            openssl
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            # Additional darwin specific inputs can be set here
+            pkgs.libiconv
+          ];
+        };
+
+        simple_update_checker = craneLib.buildPackage (commonArgs // {
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+          # Additional environment variables or build phases/hooks can be set
+          # here *without* rebuilding all dependency crates
+          # MY_CUSTOM_VAR = "some value";
+        });
+      in
       {
-        inherit inputs;
-      }
-      {
-        systems = [
-          "x86_64-linux"
-          "aarch64-linux"
-          "x86_64-darwin"
-          "aarch64-darwin"
-        ];
-        perSystem =
-          { config
-          , pkgs
-          , system
-          , self
-          , ...
-          }:
-          let
-            craneLib = inputs.crane.mkLib pkgs;
-            src = craneLib.cleanCargoSource (craneLib.path ./.);
-            cargoArtifacts = craneLib.buildDepsOnly { inherit src; };
-            simple_update_checker = craneLib.buildPackage {
-              inherit cargoArtifacts src;
-              # disable check because two tests fail because files can not be found (needs to be fixed, but I currently don't know how)
-              doCheck = false;
-            };
-          in
-          {
-            devShells.default = pkgs.mkShell {
-              buildInputs = with pkgs; [
-                cargo
-                gcc
-                rustfmt
-                rustc
-                clippy
-                pkg-config
-                openssl
-                sqlx-cli
-                spotdl
-              ];
+        checks = {
+          simple_update_checker = simple_update_checker;
+        };
 
-              # Certain Rust tools won't work without this
-              # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
-              # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
-              RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-            };
+        packages.default = simple_update_checker;
 
-            packages = {
+        apps.default = flake-utils.lib.mkApp {
+          drv = simple_update_checker;
+        };
 
-              default = simple_update_checker;
+        devShells.default = craneLib.devShell {
+          # Inherit inputs from checks.
+          checks = self.checks.${system};
 
-            };
+          # Additional dev-shell environment variables can be set directly
 
-          };
+          # certain rust tools won't work without this
+          # this can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
+          # see https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 
-      };
+          # Extra inputs can be added here; cargo and rustc are provided by default.
+          packages = with pkgs; [
+            cargo
+            gcc
+            rustfmt
+            rustc
+            clippy
+            pkg-config
+            openssl
+            sqlx-cli
+          ];
+        };
+      });
 }
