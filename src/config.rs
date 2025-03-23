5b0ecf09968468a783d750f097fdb06da319a2e4
LMH01 <louis.haensch01@gmail.com>
2025-03-23 11:58:36 +0100

    removed hardcoding of config path

--- src/config.rs
+++ src/config.rs
@@ -6,6 +6,9 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct Config {
+    /// Path where the config file was found.
+    #[serde(skip)]
+    pub path: String,
     pub db_path: String,
 }
 
@@ -28,7 +31,8 @@ impl Config {
             return Ok(None);
         }
 
-        let config = toml::from_str::<Config>(&fs::read_to_string(config_file)?)?;
+        let mut config = toml::from_str::<Config>(&fs::read_to_string(&config_file)?)?;
+        config.path = config_file.as_path().to_str().unwrap_or("File path could not be determined").to_string();
 
         Ok(Some(config))
     }
